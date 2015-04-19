d831383730b52581e594b40ebe58d063bb0c0ccb
bridgar <bridgerhahn@gmail.com>
2015-04-19 13:24:26 -0400

    Licensing

--- 0adProcessor/Map/map-gen.py
+++ 0adProcessor/Map/map-gen.py
@@ -1,3 +1,19 @@
+#	Castle is a covert channel for use with 0 A.D.
+#    Copyright (C) 2015  Bridger Hahn
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 #Generates a 0ad map with units spaced according to input constraints.
 #inputs: x_start, z_start, x_max, z_max, x_space, z_space, template, orientation, seed, filename
 
